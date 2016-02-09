fe399faa892fe497724ddce1c4741a87e2b98004
xzaleksey <xaleksey@gmail.com>
2016-02-09 15:37:47 +0300

    Removed business logic from SyncServiceImpl to MainInteractorImpl

--- VKPhoto/app/src/main/java/com/khasang/vkphoto/domain/interactors/MainInteractorImpl.java
+++ VKPhoto/app/src/main/java/com/khasang/vkphoto/domain/interactors/MainInteractorImpl.java
@@ -1,8 +1,23 @@
 package com.khasang.vkphoto.domain.interactors;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.khasang.vkphoto.domain.interfaces.SyncServiceProvider;
 import com.khasang.vkphoto.domain.listeners.OnGetAllAlbumsListener;
+import com.khasang.vkphoto.executor.MainThread;
+import com.khasang.vkphoto.executor.MainThreadImpl;
+import com.khasang.vkphoto.model.Items;
+import com.khasang.vkphoto.model.Response;
+import com.khasang.vkphoto.model.album.PhotoAlbum;
 import com.khasang.vkphoto.services.SyncService;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.api.VKApiConst;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+
+import java.lang.reflect.Type;
 
 /**
  * Реализация интерфейса исполнителя запросов к службе синхронизации.
@@ -15,6 +30,7 @@ import com.khasang.vkphoto.services.SyncService;
 public class MainInteractorImpl implements MainInteractor {
     private SyncServiceProvider syncServiceProvider;
     private SyncService syncService;
+    private MainThread mainThread = new MainThreadImpl();
 
     public MainInteractorImpl(SyncServiceProvider syncServiceProvider) {
         this.syncServiceProvider = syncServiceProvider;
@@ -47,7 +63,44 @@ public class MainInteractorImpl implements MainInteractor {
                 return;
             }
         }
-        syncService.getAllAlbums(onGetAllAlbumsListener);
+        syncService.getAllAlbums(getAllAlbumsRunnable(onGetAllAlbumsListener));
+    }
+
+    private Runnable getAllAlbumsRunnable(final OnGetAllAlbumsListener onGetAllAlbumsListener) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                VKRequest request = new VKRequest("photos.getAlbums", VKParameters.from(VKApiConst.OWNER_ID, VKAccessToken.currentToken().userId));
+                request.executeWithListener(new VKRequest.VKRequestListener() {
+                    @Override
+                    public void onComplete(VKResponse response) {
+                        super.onComplete(response);
+                        Type photoAlbumsType = new TypeToken<Response<Items<PhotoAlbum>>>() {
+                        }.getType();
+                        final Response<Items<PhotoAlbum>> albumsResponse = new Gson().fromJson(response.json.toString(), photoAlbumsType);
+                        if (onGetAllAlbumsListener != null) {
+                            mainThread.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    onGetAllAlbumsListener.onSuccess(albumsResponse.response.results);
+                                }
+                            });
+                        }
+                    }
+
+                    @Override
+                    public void onError(final VKError error) {
+                        super.onError(error);
+                        mainThread.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                onGetAllAlbumsListener.onVKError(error);
+                            }
+                        });
+                    }
+                });
+            }
+        };
     }
 }
       
