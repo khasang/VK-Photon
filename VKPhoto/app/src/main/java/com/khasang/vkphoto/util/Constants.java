e1cbefe989e2f6cc164181e93240b248316e2f55
xzaleksey <xaleksey@gmail.com>
2016-02-12 10:57:41 +0300

    Updated PhotoAlbum and Photo classes. Use them insted of VkAPiPhoto and VkPhotoAlbum!

---
+++ VKPhoto/app/src/main/java/com/khasang/vkphoto/util/Constants.java
@@ -0,0 +1,9 @@
+package com.khasang.vkphoto.util;
+
+public class Constants {
+    public static final int SYNC_NOT_STARTED = 0;
+    public static final int SYNC_STARTED = 1;
+    public static final int SYNC_SUCCESS = 2;
+    public static final int SYNC_FAILED = 3;
+    public static final int SYNC_DELETED = -1;
+}
