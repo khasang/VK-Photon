328832ab74f96be0372ae0b5cdc815cb762553b6
gostevanton <gostevanton@yandex.ru>
2016-02-22 13:47:49 +0300

    add method deletePhotoById

--- VKPhoto/app/src/main/java/com/khasang/vkphoto/presentation/presenter/VKPhotosPresenter.java
+++ VKPhoto/app/src/main/java/com/khasang/vkphoto/presentation/presenter/VKPhotosPresenter.java
@@ -2,4 +2,6 @@ package com.khasang.vkphoto.presentation.presenter;
 
 public interface VKPhotosPresenter extends Presenter {
     void getPhotosByAlbumId(int albumId);
+
+    void deletePhotoById(int photoId);
 }
