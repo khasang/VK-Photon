cc2a4b632e0604d58d660c05694ecfbd5e4c205e
Vladimir Polkhovtsev <bugtsa@gmail.com>
2016-02-24 12:01:24 +0300

    fix conflict

--- VKPhoto/app/src/main/java/com/khasang/vkphoto/data/vk/VKAlbumSource.java
+++ VKPhoto/app/src/main/java/com/khasang/vkphoto/data/vk/VKAlbumSource.java
@@ -3,14 +3,12 @@ package com.khasang.vkphoto.data.vk;
 import com.khasang.vkphoto.data.RequestMaker;
 import com.khasang.vkphoto.domain.events.ErrorEvent;
 import com.khasang.vkphoto.domain.events.GetVKAlbumsEvent;
-import com.khasang.vkphoto.domain.events.GetVkSaveAlbumEvent;
 import com.khasang.vkphoto.presentation.model.PhotoAlbum;
 import com.khasang.vkphoto.util.JsonUtils;
 import com.khasang.vkphoto.util.Logger;
 import com.vk.sdk.api.VKError;
 import com.vk.sdk.api.VKRequest;
 import com.vk.sdk.api.VKResponse;
-import com.vk.sdk.api.model.VKPrivacy;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -30,36 +28,8 @@ public class VKAlbumSource {
      */
     public void addAlbum(final String title, final String description,
                          final Vector<String> listUploadedFiles, final int privacy, final int comment_privacy) {
-//        requestAddAlbum(title, description, listUploadedFiles, privacy, comment_privacy);
+        requestMaker.requestAddAlbum(title, description, listUploadedFiles, privacy, comment_privacy);
     }
-
-//    public void requestAddAlbum(String title, String description, int privacy, int commentPrivacy) {
-//        requestMaker.createEmptyAlbum(new VKRequest.VKRequestListener() {
-//            @Override
-//            public void onComplete(VKResponse response) {
-//                super.onComplete(response);
-//                final PhotoAlbum photoAlbum;
-//                try {
-//                    photoAlbum = JsonUtils.getJsonTest(response.json);
-//                    Logger.d("Create Album successfully");
-//                    EventBus.getDefault().postSticky(new GetVkSaveAlbumEvent(photoAlbum));
-//                } catch (Exception e) {
-//                    sendError(e.toString());
-//                }
-//            }
-//
-//            @Override
-//            public void onError(VKError error) {
-//                super.onError(error);
-//                sendError(error.toString());
-//            }
-//
-//            void sendError(String s) {
-//                EventBus.getDefault().postSticky(new ErrorEvent(s));
-//            }
-//        }, "Test 16.02", "test album", VKPrivacy.PRIVACY_FRIENDS, VKPrivacy.PRIVACY_NOBODY);
-//    }
-
 
     public void updateAlbum() {
 
