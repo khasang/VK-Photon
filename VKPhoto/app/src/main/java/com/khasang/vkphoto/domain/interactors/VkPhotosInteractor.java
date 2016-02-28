922e6ac00a1386eda97294a71f83f29e493d83aa
gostevanton <gostevanton@yandex.ru>
2016-02-28 21:36:14 +0300

    rename deleteSelectedPhotos

--- VKPhoto/app/src/main/java/com/khasang/vkphoto/domain/interactors/VkPhotosInteractor.java
+++ VKPhoto/app/src/main/java/com/khasang/vkphoto/domain/interactors/VkPhotosInteractor.java
@@ -1,10 +1,15 @@
 package com.khasang.vkphoto.domain.interactors;
 
+import com.bignerdranch.android.multiselector.MultiSelector;
+import com.khasang.vkphoto.presentation.model.Photo;
+
+import java.util.List;
+
 /**
  * Created by Anton on 21.02.2016.
  */
 public interface VkPhotosInteractor {
     void getPhotosByAlbumId(int albumId);
 
-    void deletePhotoById(int photoId);
+    void deleteSelectedVkPhotos(MultiSelector multiSelector, List<Photo> photoList);
 }
