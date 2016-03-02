6aa6acf1ecb8dbcfd40bc55997bfb58705f398e4
xzaleksey <xaleksey@gmail.com>
2016-03-02 16:58:28 +0300

    localAlbums working partially.

--- VKPhoto/app/src/main/java/com/khasang/vkphoto/data/LocalAlbumsCursorLoader.java
+++ VKPhoto/app/src/main/java/com/khasang/vkphoto/data/LocalAlbumsCursorLoader.java
@@ -2,11 +2,17 @@ package com.khasang.vkphoto.data;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.net.Uri;
+import android.provider.BaseColumns;
 import android.provider.MediaStore;
 import android.util.Log;
+
+import com.khasang.vkphoto.data.database.tables.PhotoAlbumsTable;
 import com.khasang.vkphoto.data.local.LocalAlbumSource;
-import com.khasang.vkphoto.presentation.model.PhotoAlbum;
+import com.khasang.vkphoto.util.ImageFileFilter;
+
+import java.io.File;
 
 public class LocalAlbumsCursorLoader extends android.support.v4.content.CursorLoader {
     private LocalAlbumSource localAlbumSource;
@@ -18,18 +24,12 @@ public class LocalAlbumsCursorLoader extends android.support.v4.content.CursorLo
 
     @Override
     public Cursor loadInBackground() {
-//        List<PhotoAlbum> localAlbumsList = localAlbumSource.getAllLocalAlbums();
-//        String[] columns = new String[] { "_id", "item"};
-//        MatrixCursor matrixCursor= new MatrixCursor(columns);
-//        getActivity().startManagingCursor(matrixCursor);
-//        for (int i = 0; i < localAlbumsList.size(); i++)
-//            matrixCursor.addRow(new Object[]{i, localAlbumsList.get(i)});
-//
-//        Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-//        String[] projection = {MediaStore.MediaColumns.DATA};
-//        Cursor cursor = getContext().getContentResolver().query(uri, projection, null, null, null);
-
-
+        MatrixCursor matrixCursor = new MatrixCursor(new String[]{BaseColumns._ID,
+                PhotoAlbumsTable.TITLE,
+                PhotoAlbumsTable.FILE_PATH,
+                PhotoAlbumsTable.THUMB_FILE_PATH,
+                PhotoAlbumsTable.SIZE});
+        MatrixCursor.RowBuilder builder;
         Uri images = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
         String[] PROJECTION_BUCKET = {
                 MediaStore.Images.ImageColumns.BUCKET_ID,
@@ -51,11 +51,12 @@ public class LocalAlbumsCursorLoader extends android.support.v4.content.CursorLo
         Cursor cursor = getContext().getContentResolver().query(
                 images, PROJECTION_BUCKET, BUCKET_GROUP_BY, null, BUCKET_ORDER_BY);
 
-        try { Log.i("ListingImages", " query count=" + cursor.getCount());}
-        catch (NullPointerException e) {/*NOP*/}
+        try {
+            Log.i("ListingImages", " query count=" + cursor.getCount());
+        } catch (NullPointerException e) {/*NOP*/}
 
         if (cursor.moveToFirst()) {
-            String bucketID, bucketName, date, data;
+            String bucketID, bucketName, date, thumbPath;
             int bucketIDColumn = cursor.getColumnIndex(MediaStore.Images.Media.BUCKET_ID);
             int bucketNameColumn = cursor.getColumnIndex(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
             int dateColumn = cursor.getColumnIndex(MediaStore.Images.Media.DATE_TAKEN);
@@ -63,18 +64,25 @@ public class LocalAlbumsCursorLoader extends android.support.v4.content.CursorLo
 
             do {
                 // Get the field values
+                builder = matrixCursor.newRow();
                 bucketID = cursor.getString(bucketIDColumn);
+                builder.add(bucketID);
                 bucketName = cursor.getString(bucketNameColumn);
+                builder.add(bucketName);
+                thumbPath = cursor.getString(dataColumn);
+                String filePath = thumbPath.substring(0, thumbPath.lastIndexOf("/"));
+                builder.add(filePath);
+                builder.add(thumbPath);
+                builder.add(new File(filePath).listFiles(new ImageFileFilter()).length);
                 date = cursor.getString(dateColumn);
-                data = cursor.getString(dataColumn);
-
                 // Do something with the values.
                 Log.i("ListingImages", " bucket=" + bucketID
                         + "  bucketName=" + bucketName
                         + "  date_taken=" + date
-                        + "  _data=" + data);
+                        + "  _data=" + thumbPath);
             } while (cursor.moveToNext());
         }
-        return cursor;
+
+        return matrixCursor;
     }
 }
