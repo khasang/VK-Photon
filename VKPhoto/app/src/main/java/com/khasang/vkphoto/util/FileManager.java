ddd7f198f24fce46efd616d2d6af4b8c2ee788ba
xzaleksey <xaleksey@gmail.com>
2016-03-17 18:24:59 +0300

    added file futures.

--- VKPhoto/app/src/main/java/com/khasang/vkphoto/util/FileManager.java
+++ VKPhoto/app/src/main/java/com/khasang/vkphoto/util/FileManager.java
@@ -105,7 +105,7 @@ public class FileManager {
             InputStream input = new BufferedInputStream(url.openStream());
             OutputStream output = new FileOutputStream(filePath, false);
             byte data[] = new byte[1024];
-            while ((count = input.read(data)) != -1) {
+            while ((count = input.read(data)) != -1 && !Thread.currentThread().isInterrupted()) {
                 //      total += count;
 //                publishProgress((int) (total * 100 / lenghtOfFile));
                 output.write(data, 0, count);
